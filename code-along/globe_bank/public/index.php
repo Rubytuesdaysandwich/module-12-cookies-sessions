e16fd1da3f0fc73ad33c566b00c732f9214512cc
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-22 15:51:33 -0600

    update

--- code-along/globe_bank/public/index.php
+++ code-along/globe_bank/public/index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Globe Bank</title>
-  </head>
-  <body>
-      <h1>Globe Bank: Coming Soon</h1>
-  </body>
-</html>
+<?php require_once('../private/initialize.php'); ?>
+
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page">
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
